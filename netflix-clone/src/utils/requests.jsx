3a21bb2f924aa0ed6f09b079ba9b5f5f9c56d19b
meron shola <meron@merons-MBP.lan>
2025-02-26 14:15:37 -0600

    deploy

--- netflix-clone/src/utils/requests.jsx
+++ netflix-clone/src/utils/requests.jsx
@@ -1,5 +1,5 @@
 
-const API_KEY = "b1db857746b600bff8066e3a00cacba1"
+const API_KEY = process.env.REACT_APP_DB_API_KEY
 
     const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
